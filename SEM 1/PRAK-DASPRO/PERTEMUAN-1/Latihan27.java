f398648fa11d7a4043a2e70526fc5fde05c516e4
Rakai Seto <mamangrakai@gmail.com>
2023-09-01 16:39:24 +0700

    1 Sept 2023

---
+++ SEM 1/PRAK-DASPRO/PERTEMUAN-1/Latihan27.java
@@ -0,0 +1,6 @@
+public class Latihan27 {
+    public static void main(String[] args) {
+        System.out.print("Nama Saya Rakai Seto Sembodo");
+        System.out.print("\nSaya adalah mahasiswa baru Jurusan Teknologi Informasi - POLINEMA");
+    }
+}
---
+++ SEM 1/PRAK-DASPRO/PERTEMUAN-1/tugas27.java
@@ -0,0 +1,13 @@
+/**
+ * tugas
+ */
+public class tugas27 {
+    public static void main(String[] args) {
+        System.out.println("***** IDENTITAS DIRI *****");    
+        System.out.println("Nama        : Rakai Seto Sembodo");    
+        System.out.println("Absen       : 27");    
+        System.out.println("Kelas       : TI 1G");    
+        System.out.println("Prodi       : D4 Teknik Informatika");    
+        System.out.println("Jurusan     : Teknologi Informasi");    
+    }
+}
